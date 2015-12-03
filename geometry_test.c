6d1a37a8d4877f5d5f2828b83ef971f0955aa34a
brobinson124 <brooke.robinson@colorado.edu>
2015-12-02 17:48:23 -0700

    peace out

--- geometry_test.c
+++ geometry_test.c
@@ -15,6 +15,18 @@
 
 #include "geometry.h"
 
+START_TEST(test_2d_area)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    
+    c.x = a.x = b.x = 0;
+    c.y = a.y = b.y = 0;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+}
+END_TEST
+
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
 {
@@ -162,10 +174,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area");
+    tcase_add_test(tc_2d_area, test_2d_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
